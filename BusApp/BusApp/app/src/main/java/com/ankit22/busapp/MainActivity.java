c1c473161f60be8368f3db4cdf7de0d14f37e839
Unknown <kirtanpatl@gmail.com>
2018-03-13 00:16:43 -0400

    Added Async

--- BusApp/BusApp/app/src/main/java/com/ankit22/busapp/MainActivity.java
+++ BusApp/BusApp/app/src/main/java/com/ankit22/busapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ankit22.busapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Base URL String
+    String baseURL = "";
+    //
+
+
+    private class BusAsyncTask extends AsyncTask<URL, Void, String>{
+
     }
 }
